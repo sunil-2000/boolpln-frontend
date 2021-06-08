81f29a3d1b4cf373339434dd32f2c122f031d4af
sunil-2000 <sunilsabnis10@gmail.com>
2021-06-08 18:51:39 -0500

    organization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Week from "./week.js";
+import Week from "./components/calendar/week.js";
 
 ReactDOM.render(<Week />, document.getElementById("root"));
