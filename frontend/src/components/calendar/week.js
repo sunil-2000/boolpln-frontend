4b3b506d24395cb0774814e89fce893baa89d609
sunil-2000 <sunilsabnis10@gmail.com>
2021-06-09 13:31:24 -0500

    cleanup

--- frontend/src/components/calendar/week.js
+++ frontend/src/components/calendar/week.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Day from "./day.js";
 
+const dateMap = {
+  0: "Sun",
+  1: "Mon",
+  2: "Tues",
+  3: "Wed",
+  4: "Thurs",
+  5: "Fri",
+  6: "Sat",
+};
+const today = new Date();
+
 class Week extends React.Component {
   constructor(props) {
     super(props);
@@ -16,59 +27,82 @@ class Week extends React.Component {
       fontSize: "1em",
       textAlign: "center",
     };
-    labelsLst.push(
-      <div style={timeStyle} className="label">
-        {12 + " AM"}
-      </div>
-    );
-    for (let x = 1; x < hours; x++) {
+
+    let curHour = today.getHours();
+    for (let x = 0; x <= hours; x++) {
+      // start at current hour dervived from date-time object (builtin)
+      let dispHour;
+      if (curHour <= 11 && curHour > 0) {
+        dispHour = curHour + " AM";
+      } else if (curHour === 12) {
+        dispHour = curHour + " PM";
+      } else if (curHour === 0) {
+        dispHour = curHour + 12 + " AM";
+      } else {
+        dispHour = curHour - 12 + " PM";
+      }
+
       labelsLst.push(
-        <div style={timeStyle} className="label">
-          {x + " AM"}
+        <div style={timeStyle} className='timeslot-label'>
+          {dispHour}
         </div>
       );
+      curHour = (curHour + 1) % 24;
     }
-    console.log(labelsLst);
     return labelsLst;
   }
+
+  renderDaysWeek(days) {
+    let curDay = today.getDay();
+    let dayLst = [];
+    for (let x = 0; x < days; x++) {
+      if (x === days - 1) {
+        dayLst.push(<Day value={dateMap[curDay]} hours={13} end={true}></Day>);
+      } else {
+        dayLst.push(<Day value={dateMap[curDay]} hours={13} end={false}></Day>);
+      }
+      curDay = (curDay + 1) % 7;
+    }
+    return dayLst;
+  }
+
   render() {
     const weekStyle = {
       marginTop: "10em",
       marginLeft: "0",
     };
     const labelsStyle = {
-      marginTop: "10em",
+      // marginTop: "10em",
       justifyContent: "space-between",
       border: "1px solid #C4C4C4",
       borderRight: "none",
     };
     const timeStyle = {
-      color: "#989898",
+      borderBottom: "1px solid #C4C4C4",
+      height: "4em",
+      display: "flex",
+      justifyContent: "space-around",
+      alignItems: "flex-end",
+      fontSize: "2em",
       fontFamily: "Arial",
-      fontSize: "1em",
-      textAlign: "center",
-      marginTop: "6em",
+      color: "white",
+      paddingBottom: "0.5em",
     };
     const weekContainerStyle = {
-      float: "right",
       margin: "auto",
+      float: "right",
     };
+
     return (
-      <div style={weekContainerStyle} className="week-container">
-        <div style={labelsStyle} className="labels-container">
-          <div style={timeStyle} className="label-tag">
-            ALL DAY
+      <div style={weekContainerStyle} className='week-container'>
+        <div style={weekStyle} className='week'>
+          <div style={labelsStyle} className='labels-container'>
+            <div style={timeStyle} className='label-tag'>
+              ALLDAY
+            </div>
+            {this.renderLabels(12)}
           </div>
-          {this.renderLabels(12)}
-        </div>
-        <div style={weekStyle} className="week">
-          <Day value="Mon" hours={12}></Day>
-          <Day value="Tues" hours={12}></Day>
-          <Day value="Wed" hours={12}></Day>
-          <Day value="Thurs" hours={12}></Day>
-          <Day value="Fri" hours={12}></Day>
-          <Day value="Sat" hours={12}></Day>
-          <Day value="Sun" hours={12}></Day>
+          {this.renderDaysWeek(7)}
         </div>
       </div>
     );
