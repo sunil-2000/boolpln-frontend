8882cfcb01d6bd871e400341226abf27b248a476
Rishik Zaparde <rishikzaparde@gmail.com>
2021-06-23 17:20:26 -0400

    stuff

---
+++ frontend/src/components/groups/groups.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import classes from "../../styles/groups/groups.module.css";
+import "reactjs-popup/dist/index.css";
+
+const Groups = () => {
+  let groupData = [];
+  return (
+    <div className={classes.groupContainer}>
+      <h1>Groups</h1>
+    </div>
+  );
+};
+
+export default Groups;
