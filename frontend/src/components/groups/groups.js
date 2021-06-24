b8ff30ff8a5325811b24f5a6ab599eccc8a36b89
sunil-2000 <sunilsabnis10@gmail.com>
2021-06-24 16:46:58 -0500

    fix commiting node_modules

--- frontend/src/components/groups/groups.js
+++ frontend/src/components/groups/groups.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import classes from "../../styles/groups/groups.module.css";
-import "reactjs-popup/dist/index.css";
+// import "reactjs-popup/dist/index.css";
 
 const Groups = () => {
   let groupData = [];
