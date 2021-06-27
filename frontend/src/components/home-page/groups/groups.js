113434fc1b4227c5f3c51e26e58ce5e57fb6b27b
Rishik Zaparde <rishikzaparde@gmail.com>
2021-06-26 23:26:56 -0400

    groups changes

--- frontend/src/components/home-page/groups/groups.js
+++ frontend/src/components/home-page/groups/groups.js
@@ -1,24 +1,22 @@
 import classes from "../../../styles/groups/groups.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import "reactjs-popup/dist/index.css";
 
-let initialData = [
-  { name: "Group 1" },
-  { name: "Group 2" },
-  { name: "Group 3" },
-];
+let initialData = [];
 
-const Groups = () => {
+const Groups = ({ onClick, newGroupName, listGroups, setListGroups }) => {
+  useEffect(() => {
+    if (listGroups.length > 0) setList(listGroups);
+  });
+
   const [list, setList] = useState(initialData);
 
-  function renderGroups() {
-    return;
-  }
-
   function clicked() {
-    setList(list.concat({ name: "group n" }));
+    onClick();
+    setListGroups(list.push(listGroups));
     console.log(list);
   }
+
   return (
     <div className={classes.groupContainer}>
       <h1>Groups</h1>
@@ -26,6 +24,7 @@ const Groups = () => {
         <button className={classes.addButton} onClick={() => clicked()}>
           Add Group
         </button>
+        {newGroupName}
         {list.map((group) => (
           <div className={classes.groupBox}>
             <svg height="5em" viewBox="-1 -1 2 2">
--- frontend/src/components/home-page/popup.js
+++ frontend/src/components/home-page/popup.js
@@ -1,1 +1,41 @@
-cl;
+import classes from "../../styles/home-page/popup.module.css";
+import { useState } from "react";
+
+const Popup = ({ onClick, setListGroups }) => {
+  const [group, setGroup] = useState("");
+
+  function updateGroup(event) {
+    setGroup(event.target.value);
+  }
+
+  function updateGroupName(event) {
+    event.preventDefault();
+    setListGroups([group]);
+  }
+
+  return (
+    <div className={classes.modal}>
+      <div className={classes.modal_content}>
+        <span className={classes.close} onClick={onClick}>
+          &times;
+        </span>
+        <form onSubmit={updateGroupName}>
+          <h3>Create Your Group</h3>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={group}
+              onChange={updateGroup}
+              required="required"
+            />
+          </label>
+          <br />
+          <input type="Submit" value="submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
