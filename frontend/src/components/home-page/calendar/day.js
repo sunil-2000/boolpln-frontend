a53444881d326e69ab57f25e79025e80d1318850
mdgoldstein2 <mdgoldstein2@wisc.edu>
2021-07-18 17:50:47 -0500

    comments for days + remove side class

--- frontend/src/components/home-page/calendar/day.js
+++ frontend/src/components/home-page/calendar/day.js
@@ -5,19 +5,23 @@ import "animate.css";
 import { connect } from "react-redux";
 import { addDay } from "../../../redux/actions/dayActions";
 
+// class which represents each dat in the calendar
 class Day extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      times: Array(24).fill(""),
-      value: this.props.value,
-      end: this.props.end,
-      date: this.props.full_date,
+      times: Array(24).fill(""), // array whose entries represent which hour was selected
+      value: this.props.value, // the day of the week
+      end: this.props.end, // indicator for last of the 7 days displayed. Is it necessary?
+      date: this.props.full_date, // full date of the day
     };
   }
 
+  // function that  renders a single timeslot on each day
   renderTimeSlot(i) {
-    let on = this.state.times[i] !== "" ? true : false;
+    let on = this.state.times[i] !== "" ? true : false; // whether the timeslot is "on"
+
+    // the key should be able to be just i (debug only then)
     return (
       <Time
         fillColor={this.state.times[i]}
@@ -38,46 +42,56 @@ class Day extends React.Component {
     );
   }
 
+  // function that handles a click on a time slot
   handleClick(i) {
-    let timeSlots = this.state.times.slice();
+    let timeSlots = this.state.times.slice(); // create  a shallow copy of the array of time slots
 
+    // if slot has been selected, mark as deselected, otherwise do opposite
     if (timeSlots[i] === "") {
       timeSlots[i] = "green";
     } else {
       timeSlots[i] = "";
     }
-    // add call to update store
-    this.setState({ times: timeSlots });
+
+    this.setState({ times: timeSlots }); // set day's state
   }
-  // renderDay returns a column with hoursNo number of time slots
+
+  // renders 24 timeslots as a day
   renderDay() {
+    // load a list of 24 timeslots
     let timeLst = [];
     for (let x = 0; x < 24; x++) {
       timeLst.push(this.renderTimeSlot(x));
     }
-    return timeLst;
+
+    return timeLst; // return list
   }
 
+  // when component mounts, call redux action to save state
   componentDidMount() {
     this.props.addDay(this.state.date, this.state.times);
   }
+
+  // when component updates, call redux action to save state
   componentDidUpdate() {
     this.props.addDay(this.state.date, this.state.times);
   }
 
-  render() {
-    function handleBorderOverlap(end) {
-      if (end) return "day-tag-sun";
-      else return "day-tag";
-    }
+  // function for assigning special css. Is this necessary, and if so, should it be moved outside?
+  handleBorderOverlap(end) {
+    if (end) return "day-tag-sun";
+    else return "day-tag";
+  }
 
+  // actually renders the page
+  render() {
     return (
-      <div className='day-container'>
-        <div className={handleBorderOverlap(this.props.end)}>
-          <div className='date-num'>{this.props.date}</div>
-          <div className='date-name'>{this.props.value}</div>
+      <div className="day-container">
+        <div className={this.handleBorderOverlap(this.props.end)}>
+          <div className="date-num">{this.props.date}</div>
+          <div className="date-name">{this.props.value}</div>
         </div>
-        <div className='day-column'>
+        <div className="day-column">
           {this.renderDay()}
           {/* allow switch for am/pm, and eventually allow arg for renderDay to be derived from props*/}
         </div>
@@ -86,4 +100,4 @@ class Day extends React.Component {
   }
 }
 
-export default connect(null, { addDay })(Day);
+export default connect(null, { addDay })(Day); // attach redux action to day as a prop
